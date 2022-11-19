fb62c870e790f7cbf5650bf3deaf8fd86c7ada49
Soares <soares_123@msn.com>
2022-11-19 02:49:18 +0200

    Fix/Improve ex.9

--- Module1/ex9.js
+++ Module1/ex9.js
@@ -1,8 +1,8 @@
 const number = prompt("Type a number")
 let prime = true
 
-if (number <= 1) {
-    document.querySelector("#target").innerHTML = `<h1>Invalid input. Prime numbers must be an integer number greater than 1</h1>`;
+if (number <= 1 || isNaN(number)) {
+    document.querySelector("#target").innerHTML = `<h1>Invalid input. Prime numbers must be an integer greater than 1</h1>`;
 }
 else {
     for (let i=2; i < number; i++){
@@ -11,9 +11,9 @@ else {
             break
         }
     }
-}
-if (prime) {
-    document.querySelector("#target").innerHTML += `<h1>${number} is a prime number</h1>`;
-} else {
-    document.querySelector("#target").innerHTML += `<h1>${number} is not a prime number</h1>`;
+    if (prime) {
+        document.querySelector("#target").innerHTML += `<h1>${number} is a prime number</h1>`;
+    } else {
+        document.querySelector("#target").innerHTML += `<h1>${number} is not a prime number</h1>`;
+    }
 }
