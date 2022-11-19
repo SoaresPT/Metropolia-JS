74686c1b0aa7d45eafbbcdf7188f0997b6e61ff5
Soares <soares_123@msn.com>
2022-11-19 03:22:32 +0200

    improve ex. 10

--- Module1/ex10.js
+++ Module1/ex10.js
@@ -4,14 +4,18 @@ const repetitions = 10000
 let percentage = 0
 let rng = 0
 
-for (let i=0; i <= repetitions; i++) {
-    for (let j=1; j <= numberOfDices; j++) {
-        rng += Math.floor(Math.random() * 6 + 1);
+if (numberOfDices <= 0 || isNaN(numberOfDices) || isNaN(desiredSum)) {
+    document.querySelector("#target").innerHTML += `<h1>Invalid input for Number of Dices or Desired Sum.</h1>`;
+} else {
+    for (let i=0; i <= repetitions; i++) {
+        for (let j=1; j <= numberOfDices; j++) {
+            rng += Math.floor(Math.random() * 6 + 1);
+        }
+        if (desiredSum == rng) {
+            percentage += 1
+        }
+        rng = 0;
     }
-    if (desiredSum == rng) {
-        percentage += 1
-    }
-    rng = 0;
+    percentage = (percentage / repetitions * 100).toFixed(2);
+    document.querySelector("#target").innerHTML += `<h1>The probability of ${numberOfDices} dices rolling ${desiredSum} is ${percentage} %.<br>The dices were rolled ${repetitions} times for this sample.</h1>`;    
 }
-percentage = (percentage / repetitions * 100).toFixed(2);
-document.querySelector("#target").innerHTML += `<h1>The probability of ${numberOfDices} dices rolling ${desiredSum} is ${percentage} %.<br>The dices were rolled ${repetitions} times for this sample.</h1>`;
