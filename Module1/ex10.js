fac08644b3a24083af9eb8404ec19090ea235f30
Soares <soares_123@msn.com>
2022-11-19 02:25:18 +0200

    Fix ex 10

--- Module1/ex10.js
+++ Module1/ex10.js
@@ -2,17 +2,16 @@ const numberOfDices = Number(prompt("How many dices"))
 const desiredSum = Number(prompt("Desired sum"))
 const repetitions = 10000
 let percentage = 0
+let rng = 0
 
 for (let i=0; i <= repetitions; i++) {
-    rng =  Math.floor(Math.random() * (numberOfDices * 6)) + numberOfDices;
-    // debug only
-    //console.log(rng);
-    //document.querySelector("#target").innerHTML += `Sum: ${rng}<br>`;
+    for (let j=1; j <= numberOfDices; j++) {
+        rng += Math.floor(Math.random() * 6 + 1);
+    }
     if (desiredSum == rng) {
-        console.log(`${rng}`);
         percentage += 1
     }
+    rng = 0;
 }
-//console.log(`# of hits : ${percentage}`)
 percentage = (percentage / repetitions * 100).toFixed(2);
 document.querySelector("#target").innerHTML += `<h1>The probability of ${numberOfDices} dices rolling ${desiredSum} is ${percentage} %.<br>The dices were rolled ${repetitions} times for this sample.</h1>`;
