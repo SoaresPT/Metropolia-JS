5931567c9aed6285edc9063652e6c8146f14b599
SoaresPT <soares_123@msn.com>
2022-11-27 16:19:47 +0200

    Update ex9.js

--- Module2/ex9.js
+++ Module2/ex9.js
@@ -8,7 +8,7 @@ function even(numberArray){
     return evenArray
 }
 
-numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 evenList = even(numberList)
 console.log(`Original List: ${numberList}`);
 console.log(`Even List: ${evenList}`);
