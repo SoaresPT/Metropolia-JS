7881fd2994ea9c42b15c3ceba346ac6c09f92c32
SoaresPT <soares_123@msn.com>
2022-11-27 16:18:29 +0200

    Update ex7.js

--- Module2/ex7.js
+++ Module2/ex7.js
@@ -3,7 +3,7 @@ function rollDice(sides){
     return Math.floor(Math.random() * sides) + 1;
 }
 
-rolledNumber = 0
+let rolledNumber = 0;
 while (rolledNumber != sides) {
     rolledNumber = rollDice(sides);
     document.querySelector("#target").innerHTML += `<li>${rolledNumber}</li>`;
